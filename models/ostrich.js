1a15c1ce17b5b8fbaf6200f39d2f13913e853906
bradycruickshank <124809383+bradycruickshank@users.noreply.github.com>
2024-04-16 19:41:16 -0500

    Add detail and delete buttons to collections page

--- models/ostrich.js
+++ models/ostrich.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const ostrichSchema = mongoose.Schema({
 name: String,
-age: Number,
+age: {
+    type: Number,
+    max: [10, "max age is 10"]
+},
 feather_count: Number
 })
 module.exports = mongoose.model("Ostriches",
--- routes/ostrich.js
+++ routes/ostrich.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var passport = require('passport')
 const ostrich_controlers= require('../controllers/ostrich');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', ostrich_controlers.ostrich_view_all_Page );
@@ -17,10 +28,16 @@ router.get('/detail', ostrich_controlers.ostrich_view_one_Page);
 router.get('/create', ostrich_controlers.ostrich_create_Page);
 
 /* GET create update page */
-router.get('/update', ostrich_controlers.ostrich_update_Page);
+router.get('/update', secured, ostrich_controlers.ostrich_update_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    
 
 /* GET delete ostrich page */
 router.get('/delete', ostrich_controlers.ostrich_delete_Page);
+
+
 
 
 
